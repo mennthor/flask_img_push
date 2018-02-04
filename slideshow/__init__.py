c42a48bf1cf2044cf285cecfc29446d4b361f946
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-02-04 21:50:02 +0100

    can send images

--- slideshow/__init__.py
+++ slideshow/__init__.py
@@ -1,13 +1,18 @@
 import os
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from .database import database, Post
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["DATABASE"] = os.getenv("SLIDESHOW_DB", "slideshow.sqlite")
+app.config["IMG_DIR"] = os.getenv(
+    "SLIDESHOW_IMG_DIR", os.path.join(os.getenv("HOME"), "Pictures", "wedding")
+)
 
 
 @app.before_first_request
 def init_db():
+    os.makedirs(app.config["IMG_DIR"], exist_ok=True)
     database.init(app.config["DATABASE"])
     database.create_tables([Post], safe=True)
 
@@ -24,7 +29,17 @@ def beamer():
 
 @app.route("/posts", methods=["POST"])
 def add_post():
-    return "OK"
+    post = Post()
+    post.timestamp = datetime.utcnow()
+    post.comment = request.form["comment"]
+    ext = os.path.splitext(request.files["image"].filename)[1]
+    filename = (
+        os.path.join(app.config["IMG_DIR"], post.timestamp.isoformat()) + ext
+    )
+    request.files["image"].save(filename)
+    post.image_path = filename
+    post.save()
+    return jsonify(status="OK")
 
 
 @app.route("/posts", methods=["GET"])
