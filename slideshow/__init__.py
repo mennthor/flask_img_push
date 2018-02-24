691bb509455e9627af45e0b2056a4682b45dd632
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-02-24 11:41:09 +0100

    Added debug methods for database clearing

--- slideshow/__init__.py
+++ slideshow/__init__.py
@@ -1,10 +1,24 @@
+# coding: utf-8
+
+from __future__ import print_function
 import os
+import sys
 from flask import (Flask, render_template, jsonify, request,
                    send_from_directory, redirect, url_for, flash)
 from datetime import datetime
+import numpy as np
 
 from .database import database, Post
 from .image import resize
+
+
+# #######################################################################
+# TODO:
+# 1. Fix resize issue on iPhones (portrait vs landscape)
+# 2. Backup files on DB clear
+# 3. Resize portrait and crop landscape so we fit in 4:3
+# 4. Use flask socket-io to push new pictures to gallery instead of reload
+# #######################################################################
 
 
 # Get config from env vars
@@ -14,6 +28,21 @@ app.config["DATABASE"] = os.getenv("SLIDESHOW_DB", "slideshow.sqlite")
 app.config["IMG_DIR"] = os.getenv(
     "SLIDESHOW_IMG_DIR", os.path.join(os.getenv("HOME"), "Pictures", "wedding")
 )
+
+
+def eprint(*args, **kwargs):
+    """ `print` wrapper to do console debug prints """
+    return print(*args, file=sys.stderr, **kwargs)
+
+
+def get_max_id():
+    """ Get max id from database """
+    max_id = None
+    try:
+        max_id = np.amax([msi.id for msi in Post.select()])
+    except Exception as e:
+        eprint(e)
+    return max_id
 
 
 # Init on launch
@@ -92,7 +121,8 @@ def add_post():
 
         # Get image from form, resize and save
         img_file = request.files["image"]
-        img_resized = resize(img_file)
+        # img_resized = resize(img_file)
+        img_resized = img_file
 
         ext = os.path.splitext(request.files["image"].filename)[1]
         filename = post.timestamp.isoformat().replace(":", "_") + ext
@@ -103,8 +133,8 @@ def add_post():
         post.name = filename
         post.save()
         msg = "Successfully sent image :)"
-    except:
-        msg = "Something went wrong, maybe just try again :("
+    except Exception as e:
+        msg = e
 
     flash(msg)
     return redirect(url_for("client"))
@@ -116,7 +146,41 @@ def get_posts():
     return jsonify(posts=posts)
 
 
-# Hosted images from database, acces by full filename
-@app.route('/images/<name>')
+# Hosted images from database, access by full filename
+@app.route("/images/<name>")
 def img_host(name):
     return send_from_directory(app.config['IMG_DIR'], name)
+
+
+# Clear database site
+@app.route("/database_clear")
+def db_clear():
+    max_id = get_max_id()
+    if max_id is not None:
+        del_query = (Post.delete()
+                     .where(Post.id << np.arange(1, max_id + 1).tolist()))
+        try:
+            rows_del = del_query.execute()
+            msg = "Deleted {} rows. DB is now empty.".format(rows_del)
+            success = True
+        except Exception as e:
+            msg = e
+            success = False
+        eprint("maxid is not None. ", msg)
+    else:
+        msg = "DB was already empty, did nothing."
+        eprint("maxid is None. ", msg)
+        success = True
+
+    return render_template("clear_db.html", success=success, msg=msg)
+
+
+# Show database content
+@app.route("/database_show")
+def db_show():
+    query = Post.select()
+    s = "<h1> Database dump: </h1>"
+    for item in query:
+        s += "{}: {}".format(item.id, item.name) + "<br>"
+
+    return s
