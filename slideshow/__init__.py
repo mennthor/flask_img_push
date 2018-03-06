62da2e51e73965da72c0fe115c58246443ef4a64
Thorben Menne <thorben.menne@tu-dortmund.de>
2018-03-07 00:53:39 +0100

    First working version with socket io

--- slideshow/__init__.py
+++ slideshow/__init__.py
@@ -1,12 +1,18 @@
-# from .routes import start_server
+# coding: utf-8
 
-
+from __future__ import print_function
 import os
-from flask import Flask
+import sys
+import threading
+from flask import (Flask, render_template, jsonify, request,
+                   send_from_directory, redirect, url_for, flash)
 from flask_socketio import SocketIO
+from datetime import datetime
+import numpy as np
 
-from .routes import main as main_blueprint
-from .database import database, Post
+from .database import database, Post, get_rnd_db_entries, get_max_id
+from .image import fix_orientation
+
 
 app = Flask(__name__)
 app.secret_key = "DONTTELLANYONETHESECRETKEY"
@@ -14,8 +20,6 @@ app.config["DATABASE"] = os.getenv("SLIDESHOW_DB", "slideshow.sqlite")
 app.config["IMG_DIR"] = os.getenv("SLIDESHOW_IMG_DIR",
                                   os.path.join(os.getenv("HOME"),
                                                "Pictures", "wedding"))
-
-app.register_blueprint(main_blueprint)
 
 
 # Init app before launch
@@ -28,13 +32,146 @@ def init_app():
     # Setup database
     database.init(app.config["DATABASE"])
     database.create_tables([Post], safe=True)
+    # Set the timer to push new random content to gallery
+    start_gallery_updater()
+
+
+def start_gallery_updater():
+    """ A self starting thread to update the gallery each T seconds. """
+    t = threading.Timer(5.0, start_gallery_updater)
+    t.daemon = True
+    t.start()
+    print("Updated gallery", file=sys.stderr)
+    filenames, _ = get_rnd_db_entries(N=4)
+    URL = "http://127.0.0.1:5000/images/"
+    filenames = {i: URL + s for i, s in enumerate(filenames)}
+    socket.emit("update", {"img_tl": filenames[0],
+                           "img_bl": filenames[1],
+                           "img_tr": filenames[2],
+                           "img_br": filenames[3],
+                           })
 
 
 # Init flask SocketIO
-socketio = SocketIO()
-socketio.init_app(app)
+socket = SocketIO()
+socket.init_app(app)
+
+
+# Client mobile page
+@app.route("/")
+def client():
+    """ Client site, for sending pictures and comments """
+    return render_template("client.html", error=request.args.get("error"))
+
+
+# Passive image gallery
+@app.route("/gallery")
+def gallery():
+    """ Gallery site, for displaying sent pictures and comments """
+    # Fetch 5 images from database
+    filenames, comments = get_rnd_db_entries(N=5)
+    URL = "http://127.0.0.1:5000/images/"
+    filenames = {i: URL + s for i, s in enumerate(filenames)}
+    return render_template("gallery.html", filenames=filenames,
+                           comment=comments[2])
+
+
+# Receiver site to post new images and comments to and get DB info from
+@app.route("/posts", methods=["POST"])
+def add_post():
+    # Fill post db entry
+    URL = "http://127.0.0.1:5000/images/"
+    try:
+        post = Post()
+        post.timestamp = datetime.utcnow()
+        comment = request.form["comment"]
+        post.comment = comment
+
+        # Get image from form, resize and save
+        img_file = request.files["image"]
+        img_resized = fix_orientation(img_file)
+
+        ext = os.path.splitext(request.files["image"].filename)[1]
+        filename = post.timestamp.isoformat().replace(":", "_") + ext
+        img_path = os.path.join(app.config["IMG_DIR"], filename)
+        img_resized.save(img_path)
+
+        # Save image filename in post db and finalize
+        post.name = filename
+        post.save()
+        msg = "Successfully sent image :)"
+
+        socket.emit("new_image", {"filename": URL + filename,
+                                  "comment": comment})
+    except Exception as e:
+        msg = e
+
+    flash(msg)
+    return redirect(url_for("client"))
+
+
+@app.route("/posts", methods=["GET"])
+def get_posts():
+    posts = list(Post.select().dicts())
+    return jsonify(posts=posts)
+
+
+# Hosted images from database, access by full filename
+@app.route("/images/<name>")
+def img_host(name):
+    return send_from_directory(app.config['IMG_DIR'], name)
+
+
+# DEBUG: Clear database site
+@app.route("/database_clear")
+def db_clear():
+    max_id = get_max_id()
+    if max_id is not None:
+        del_query = (Post.delete()
+                     # .where(Post.id == max_id))
+                     .where(Post.id << np.arange(1, max_id + 1).tolist()))
+        try:
+            rows_del = del_query.execute()
+            msg = "Deleted {} rows. DB is now empty.".format(rows_del)
+            success = True
+        except Exception as e:
+            msg = e
+            success = False
+    else:
+        msg = "DB was already empty, did nothing."
+        success = True
+
+    return render_template("clear_db.html", success=success, msg=msg)
+
+
+# DEBUG: Show database content
+@app.route("/database_show")
+def db_show():
+    query = Post.select()
+    s = "<h1> Database dump: </h1>"
+    for item in query:
+        s += "{}: {}".format(item.id, item.name) + "<br>"
+
+    return s
+
+
+# DEBUG to test socket io
+# @app.route("/sender", methods=["GET", "POST"])
+# def sender():
+#     if request.method == "POST":
+#         txt = request.form["msg"]
+#         socket.emit("to_receiver", {"parameter": txt})
+#         return redirect(url_for("sender"))
+
+#     msg = request.args.get("message", "no msg yet...")
+#     return render_template("sender.html", message=msg)
+
+
+# @app.route("/receiver", methods=["GET"])
+# def receiver():
+#     return render_template("receiver.html")
 
 
 # Start the server wrapper
 def start_server():
-    socketio.run(app, host="0.0.0.0", debug=True)
+    socket.run(app, host="0.0.0.0", debug=True)
--- slideshow/database.py
+++ slideshow/database.py
@@ -2,6 +2,7 @@
 Database table setup.
 """
 
+import numpy as np
 from peewee import SqliteDatabase, Model, DateTimeField, TextField
 
 database = SqliteDatabase(None)
@@ -24,3 +25,51 @@ def get_max_id():
     except Exception:
         max_id = None
     return max_id
+
+
+def get_rnd_db_entries(N=5):
+    """
+    Pulls N random filenames and comments from the db, resamples when entries in
+    db < N and uses palceholder if db is empty yet.
+
+    Returns
+    -------
+    filenames : list
+        N filename entries, not the full paths.
+    comments: list
+        N comments, belonging to the filenames.
+    """
+    max_id = get_max_id()
+
+    if max_id is not None:
+        if max_id < N:
+            # Need to show with replacement because too few images
+            ids = np.random.choice(np.arange(1, max_id + 1),
+                                   replace=True, size=N)
+            # These are returned unique so we have to rebroadcast them again
+            query = Post.select().where(Post.id << ids.tolist())
+            filenames = [msi.name for msi in query]
+            comments = [msi.comment for msi in query]
+            # Build a mapping from ids to [0, 1, 2, ...]
+            n_ids = len(filenames)
+            assert n_ids == len(np.unique(ids))
+            id_map = {i: _id for i, _id in zip(np.unique(ids),
+                                               np.arange(n_ids))}
+            _ids = np.array([id_map[i] for i in ids])
+            filenames = np.array(filenames)[_ids]
+            comments = np.array(comments)[_ids]
+        else:
+            ids = np.random.choice(np.arange(1, max_id + 1),
+                                   replace=False, size=N).tolist()
+            # Fetch names from db and pass to template
+            query = Post.select().where(Post.id << ids)
+            ids = np.arange(N)
+            np.random.shuffle(ids)
+            filenames = np.array([msi.name for msi in query])[ids]
+            comments = np.array([msi.comment for msi in query])[ids]
+    else:
+        # No imgs added yet or eror, only show placeholder
+        filenames = N * ["_placeholder_.jpg"]
+        comments = N * ["comment"]
+
+    return filenames, comments
